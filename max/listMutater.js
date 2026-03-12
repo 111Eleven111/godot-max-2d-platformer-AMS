870c293881a95772eb3f587ed835b05a369ded1d
Bagus-Eleven <bagus.andre@protonmail.com>
2026-03-12 11:00:19 +0100

    Added simple stocastic rythm algorithms for testing

--- max/listMutater.js
+++ max/listMutater.js
@@ -1,4 +1,4 @@
-// lets
+// in- and outlets
 inlets = 1;
 outlets = 1;
 
@@ -7,52 +7,103 @@ outlets = 1;
 var drum_pitches = [];
 
 // functions
-function randomMinor() {
+
+function onlyKickDrumPitches() {
     var result = [];
-    var randomIndex = 0; // Return a random integer from 0 to 10 (both included):
-
+    
     for (var i = 0; i < arguments.length; i++) {
-        randomIndex = Math.floor(Math.random() * minor.length);
-        randomPitch = minor[randomIndex];
-        result.push(arguments[i] + randomPitch);
-    }
-    outlet(0, result);
-}
-
-function randomMinor2() {
-    var result = [];
-    var randomIndex = 0; // Return a random integer from 0 to 10 (both included):
-
-    for (var i = 0; i < arguments.length; i++) {
-        randomIndex = Math.floor(Math.random() * minor2.length);
-        randomPitch = minor2[randomIndex];
-        result.push(arguments[i] + randomPitch);
-    }
-    outlet(0, result);
-}
-
-function randomVelocities() {
-    var result = [];
-    // Read arguments and randomly set velocity to be either 0 or 127
-    for (var i = 0; i < arguments.length; i++) {
-        if (Math.random() < 0.5) {
-            result.push(0);
-        } else {
-            result.push(127);
+        // arguments[i] = 36
+        
+        if ((Math.random() * 10) < 5) {
+            result.push(36);
+        }
+        else {
+            result.push(1);
         }
     }
-    outlet(0, result);
+    outlet(0, result)
 }
 
-function someVelocities() {
+function onlySnareFiftyFity() {
     var result = [];
-    // Read arguments and set every forth index tp 127 with a 50/50 chanse, others to 0
+    
     for (var i = 0; i < arguments.length; i++) {
-        if (i % 4 === 0) {
-            result.push(Math.random() < 0.5 ? 127 : 0);
-        } else {
-            result.push(0);
+        // arguments[i] = 36
+        
+        if ((Math.random() * 10) < 5) {
+            result.push(38);
+        }
+        else {
+            result.push(1);
         }
     }
-    outlet(0, result);
+    outlet(0, result)
 }
+
+function onlyHiHattFiftyFifty() {
+    var result = [];
+    
+    for (var i = 0; i < arguments.length; i++) {
+        
+        if ((Math.random() * 10) < 5) {
+            result.push(67);
+        }
+        else {
+            result.push(1);
+        }
+    }
+    outlet(0, result)
+}
+
+
+// legacy pitch generation functions:
+
+// function randomMinor() {
+//     var result = [];
+//     var randomIndex = 0; // Return a random integer from 0 to 10 (both included):
+
+//     for (var i = 0; i < arguments.length; i++) {
+//         randomIndex = Math.floor(Math.random() * minor.length);
+//         randomPitch = minor[randomIndex];
+//         result.push(arguments[i] + randomPitch);
+//     }
+//     outlet(0, result);
+// }
+
+// function randomMinor2() {
+//     var result = [];
+//     var randomIndex = 0; // Return a random integer from 0 to 10 (both included):
+
+//     for (var i = 0; i < arguments.length; i++) {
+//         randomIndex = Math.floor(Math.random() * minor2.length);
+//         randomPitch = minor2[randomIndex];
+//         result.push(arguments[i] + randomPitch);
+//     }
+//     outlet(0, result);
+// }
+
+// function randomVelocities() {
+//     var result = [];
+//     // Read arguments and randomly set velocity to be either 0 or 127
+//     for (var i = 0; i < arguments.length; i++) {
+//         if (Math.random() < 0.5) {
+//             result.push(0);
+//         } else {
+//             result.push(127);
+//         }
+//     }
+//     outlet(0, result);
+// }
+
+// function someVelocities() {
+//     var result = [];
+//     // Read arguments and set every forth index tp 127 with a 50/50 chanse, others to 0
+//     for (var i = 0; i < arguments.length; i++) {
+//         if (i % 4 === 0) {
+//             result.push(Math.random() < 0.5 ? 127 : 0);
+//         } else {
+//             result.push(0);
+//         }
+//     }
+//     outlet(0, result);
+// }
